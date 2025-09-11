e51a0aadb26dc4f2eabe7453e3ef5fee68320111
desivar <jillian_desire@yahoo.es>
2025-09-11 11:02:37 -0600

    fixed  week02/code/PriorityQueue.cs

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -24,14 +24,15 @@
 
         // Find the index of the item with the highest priority to remove
         var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++)
+        for (int index = 1; index < _queue.Count; index++) // Fixed: removed "- 1"
         {
-            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
+            if (_queue[index].Priority > _queue[highPriorityIndex].Priority) // Fixed: changed >= to >
                 highPriorityIndex = index;
         }
 
         // Remove and return the item with the highest priority
         var value = _queue[highPriorityIndex].Value;
+        _queue.RemoveAt(highPriorityIndex); // Fixed: added this line to actually remove the item
         return value;
     }
 
